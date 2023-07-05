935bec04f2c952a7ddd596ae9554286d5ce1808f
soremi kayode <banabaz.sk@gmail.com>
2023-07-05 20:23:05 +0100

    This is the first commit of the tutorial

---
+++ index.js
@@ -0,0 +1,50 @@
+// declaring many variable on a line without assigning it
+var a, b, c, d;
+a = 23;
+
+var studentName = "student";
+
+/*
+in javascript we use the camel case naming convention alot
+// camel case
+
+some of the method in javascript that uses the camels case naming
+convention
+
+document.getElementById()
+document.querySelector()
+document.getElementsByTagName()
+/*
+
+// declaring and assigning many variable on a line
+var f = 20; var g = 21; var h = 22;
+console.log(f);
+console.log(g);
+console.log(h);
+// var a = 23;
+// a = 23;
+// b = 24;
+// c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+if(a > 10){
+console.log("Yes, it;'s greater");
+}
+else {
+console.log("No, it;'s not greater");
+}
+
+/*
+try
+catch
+finally
+var
+if
+else
+for
+switch
+constant
+let
+*/
---
+++ syntax.js
@@ -0,0 +1,23 @@
+// Three ways to create a variable
+var a = 23;
+let b = 30;
+const c = 41;
+
+// Javasript number
+
+let d = 20;
+
+// Javascript string 
+
+let namofMan = 'Daniel'; 
+let namofMan2 = "Daniel";
+let f = "2.5";
+let firstPhrase = "lorem ipsum dolor sit amet, consect";
+let secondPhrase = "lorem ipsum dolor sit amet";
+
+// Javascript operators 
+
+console.log(a + c);
+console.log(firstPhrase + " " + secondPhrase);
+console.log(a + f);
+
